093f61a6f8066dc2ae122248b288f78704511daa
La Prise <gaurondorian@gmail.com>
2020-07-10 23:49:59 +0200

    route index fait

--- server_flask.py
+++ server_flask.py
@@ -1,7 +1,34 @@
-from flask import Flask,render_template
-from OBJ_Essence import find_station
+from flask import Flask,render_template,request,url_for,redirect
+from OBJ_Essence import *
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route('/',methods=['GET','POST'])
+def index():
+    if request.method == "POST":
+        try:
+            carburant = request.form["Gazole"]
+
+        except KeyError:
+            pass
+
+        try:
+            carburant = request.form["SP98"]
+        except KeyError:
+            pass
+
+        try:
+            carburant = request.form["SP95"]
+        except KeyError:
+            pass
+
+        essence = Essence(carburant)
+        essence.find_station()
+
+        return redirect(url_for("map"))
+
     return render_template("index.html")
+
+@app.route('/map')
+def map():
+    print("salut")
+    return render_template("map.html")
